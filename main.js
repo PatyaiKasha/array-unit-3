ea9eb9810bb308fd63137398657aa219ef8df681
Pavel Viznitsa <viznitsap@gmail.com>
2017-09-07 19:08:02 +0300

    complete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // #1. Напишите функцию для вывода элементов
 // массива в элемент <p id=”new”></p>.
 
-var newMas1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var newMas1 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 function masOut1() {
     var out = '';
@@ -18,36 +18,91 @@ masOut1();
 // индекс массива и значение. По нажатию
 // “ОК” - массив выводится.
 
-
 var btn2 = document.getElementById('btn2').onclick = masOut2;
 
 function masOut2() {
-    var newMas2 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     var in21 = document.getElementById('inOne').value; // mass index
     var in22 = document.getElementById('inTwo').value; // mass value
-    newMas2[in21] = in22;
+    newMas1[in21] = in22;
     var out = '';
-    for (var i = 0; i < newMas2.length; i++) {
-        out += i + ') ' + newMas2[i] + '<br>';
-        document.getElementById('out2').innerHTML = out + '<hr>';
-    }
-    console.log(newMas2);
 
-    // 3. Добавьте кнопку pop реализующую метод pop()
-    // для введенного выше массива.
-
-    var btn3 = document.getElementById('btn3').onclick = innerMasOut2;
-
-    function innerMasOut2() {
-        newMas2.pop();
-        console.log(newMas2);
-        var out = '';
-        for (var i = 0; i < newMas2.length; i++) {
-            out += i + ') ' + newMas2[i] + '<br>';
+    if (in21 != '' && in22 != '') {
+        for (var i = 0; i < newMas1.length; i++) {
+            out += i + ') ' + newMas1[i] + '<br>';
             document.getElementById('out2').innerHTML = out + '<hr>';
         }
+        console.log(newMas1);
     }
-    if (in21 < 0) { // и еще много всяких проверок ...
-        alert('Индекс массива не может быть отрицательным числом');
+}
+
+// 3. Добавьте кнопку pop реализующую метод pop()
+// для введенного выше массива.
+// Метод pop() - удаляет с конца массива
+
+var btn3 = document.getElementById('btn3').onclick = masOut3;
+
+function masOut3() {
+    newMas1.pop();
+    console.log(newMas1);
+    var out = '';
+    for (var i = 0; i < newMas1.length; i++) {
+        out += i + ') ' + newMas1[i] + '<br>';
+        document.getElementById('out2').innerHTML = out + '<hr>';
     }
+}
+
+// 4. Добавьте кнопку shift реализующую метод shift()
+// для введенного выше массива.
+// Метод shift() - удаляет с начала массива
+
+var btn4 = document.getElementById('btn4').onclick = masOut4;
+
+function masOut4() {
+    newMas1.shift();
+    console.log(newMas1);
+    var out = '';
+    for (var i = 0; i < newMas1.length; i++) {
+        out += i + ') ' + newMas1[i] + '<br>';
+        document.getElementById('out2').innerHTML = out + '<hr>';
+    }
+}
+
+// 5. Добавьте кнопку push реализующую метод push()
+// для введенного выше массива. Данные для записи
+// необходимо считывать из input.
+// Метод push() - добавляет элемент в конец массива
+
+var btn5 = document.getElementById('btn5').onclick = masOut5;
+
+function masOut5() {
+    var in21 = document.getElementById('inOne').value; // mass index
+    var in22 = document.getElementById('inTwo').value; // mass value
+
+    newMas1.push(in22);
+
+    var out = '';
+    for (var i = 0; i < newMas1.length; i++) {
+        out += i + ') ' + newMas1[i] + '<br>';
+        document.getElementById('out2').innerHTML = out + '<hr>';
+    }
+    console.log(newMas1);
+}
+// 6. Добавьте кнопку unshift реализующую метод unshift()
+//  для введенного выше массива. Данные для записи необходимо считывать из input.
+// Метод unshift() - добавляет элемент в начало массива
+
+var btn6 = document.getElementById('btn6').onclick = masOut6;
+
+function masOut6() {
+    var in21 = document.getElementById('inOne').value; // mass index
+    var in22 = document.getElementById('inTwo').value; // mass value
+
+    newMas1.unshift(in22);
+
+    var out = '';
+    for (var i = 0; i < newMas1.length; i++) {
+        out += i + ') ' + newMas1[i] + '<br>';
+        document.getElementById('out2').innerHTML = out + '<hr>';
+    }
+    console.log(newMas1);
 }
